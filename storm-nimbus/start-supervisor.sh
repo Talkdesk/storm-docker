7b1eb90a3fa770bc5d738af8b7978e08470b928e
root <root@ip-172-31-53-58.ec2.internal>
2016-04-01 18:07:12 +0000

    Connected nimbus to ZooKeeper server

--- storm-nimbus/start-supervisor.sh
+++ storm-nimbus/start-supervisor.sh
@@ -1,5 +1,5 @@
 export IP=`hostname -i`
-sed -i -e "s/%zookeeper%/$ZK_PORT_2181_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
+sed -i -e "s/%zookeeper%/ec2-52-91-200-252.compute-1.amazonaws.com/g" $STORM_HOME/conf/storm.yaml
 sed -i -e "s/%nimbus%/$IP/g" $STORM_HOME/conf/storm.yaml
 
 echo "storm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
