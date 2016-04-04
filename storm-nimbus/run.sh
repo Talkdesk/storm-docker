ec68b58c30c51d1eb977b29de2de1572d5aa3bb8
Pedro Domingues <pedro.domingues@talkdesk.com>
2016-04-04 11:32:17 +0100

    Created common docker-compose files

--- storm-nimbus/run.sh
+++
@@ -1,1 +0,0 @@
-docker run -d --name nimbus1 -p 3772-3773:3772-3773/tcp -p 6627:6627/tcp talkdesk/storm-nimbus:0.10.0
--- storm-ui/run.sh
+++
@@ -1,1 +0,0 @@
-docker run -d --link nimbus1:nimbus  -p 8080:8080/tcp talkdesk/storm-ui:0.10.0
