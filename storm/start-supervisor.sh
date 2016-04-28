9037a16f53a65545c58ea0d54b532769a349a5e7
Pedro Domingues <pedro.domingues@talkdesk.com>
2016-04-28 14:17:43 +0100

    Fixing nimbus hostname.

--- storm/start-supervisor.sh
+++ storm/start-supervisor.sh
@@ -1,6 +1,7 @@
 sed -i -e "s/%zookeeper%/ec2-52-91-200-252.compute-1.amazonaws.com/g" $STORM_HOME/conf/storm.yaml
 sed -i -e "s/%nimbus%/ec2-54-172-105-234.compute-1.amazonaws.com/g" $STORM_HOME/conf/storm.yaml
 
+echo -e "\n"
 echo "storm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
 
 /usr/sbin/sshd && supervisord
