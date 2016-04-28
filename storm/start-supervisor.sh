c628ab0361e2120a1573cf6f13e7317c28baf1b0
Pedro Domingues <pedro.domingues@talkdesk.com>
2016-04-28 14:58:02 +0100

    Revert "Solved dns."
    This reverts commit de3f1b9a2936ac9486ca6c7bb86c869e030ac483.

--- storm/start-supervisor.sh
+++ storm/start-supervisor.sh
@@ -1,5 +1,5 @@
-sed -i -e "s/%zookeeper%/52.91.200.252/g" $STORM_HOME/conf/storm.yaml
-sed -i -e "s/%nimbus%/54.172.105.234/g" $STORM_HOME/conf/storm.yaml
+sed -i -e "s/%zookeeper%/ec2-52-91-200-252.compute-1.amazonaws.com/g" $STORM_HOME/conf/storm.yaml
+sed -i -e "s/%nimbus%/ec2-54-172-105-234.compute-1.amazonaws.com/g" $STORM_HOME/conf/storm.yaml
 
 echo -e "\n"
 echo "storm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
