d8d242cb693ee4dc6348acd6c76b2870bbffaf59
Pedro Domingues <pedro.domingues@talkdesk.com>
2016-04-28 15:20:52 +0100

    Enhanced setup script.

--- storm/start-supervisor.sh
+++ storm/start-supervisor.sh
@@ -1,7 +1,41 @@
-sed -i -e "s/%zookeeper%/ec2-52-91-200-252.compute-1.amazonaws.com/g" $STORM_HOME/conf/storm.yaml
-sed -i -e "s/%nimbus%/ec2-54-172-105-234.compute-1.amazonaws.com/g" $STORM_HOME/conf/storm.yaml
+zookeeper_ip = "ec2-52-91-200-252.compute-1.amazonaws.com"
+nimbus_ip = "ec2-54-172-105-234.compute-1.amazonaws.com"
+
+# Test an IP address for validity:
+# Usage:
+#      valid_ip IP_ADDRESS
+#      if [[ $? -eq 0 ]]; then echo good; else echo bad; fi
+#   OR
+#      if valid_ip IP_ADDRESS; then echo good; else echo bad; fi
+#
+function valid_ip()
+{
+    local  ip=$1
+    local  stat=1
+
+    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        OIFS=$IFS
+        IFS='.'
+        ip=($ip)
+        IFS=$OIFS
+        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+            && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+        stat=$?
+    fi
+    return $stat
+}
+
+sed -i -e "s/%zookeeper%/$zookeeper_ip/g" $STORM_HOME/conf/storm.yaml
+sed -i -e "s/%nimbus%/$nimbus_ip/g" $STORM_HOME/conf/storm.yaml
 
 echo -e "\n"
-echo "storm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
+
+if valid_ip $nimbus_ip; then myhostname = $nimbus_ip; else myhostname = `host $nimbus_ip | awk '{ print $4 }'`; fi
+
+echo "storm.local.hostname: $myhostname" >> $STORM_HOME/conf/storm.yaml
+
+echo "#########################################"
+cat $STORM_HOME/conf/storm.yaml
+echo "#########################################"
 
 /usr/sbin/sshd && supervisord
